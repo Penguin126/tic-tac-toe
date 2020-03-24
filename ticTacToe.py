7c7fbdc12be4c27d12f4d906640a861a2628c6a4
Penguin126 <ray.wu.wang@gmail.com>
2020-03-24 12:11:32 -0400

    Added checking of invalid inputs

--- ticTacToe.py
+++ ticTacToe.py
@@ -34,22 +34,40 @@ def drawSymbol(r,c,symbol):
             display[srow+2+i][scol] = "o"
         for i in range(5):
             display[srow+2+i][scol+8] = "o"
-
+def isLegal(x,y,symbol,board):
+    if x>2 or x<0 or y>2 or y<0:
+        return False
+    elif board[x][y]!=" ":
+        return False
+    else:
+        return True
 end = False
 printDisplay()
 while(not end):
-    x = input("player x type coordinate (x,y)")
-    coordinates = x.split(",")
-    drawSymbol(int(coordinates[0])-1,int(coordinates[1])-1,"x")
-    for i in range(4):
-        print()
-    printDisplay()
-    o = input("player o type coordinate (x,y)")
-    coordinates = o.split(",")
-    drawSymbol(int(coordinates[0])-1, int(coordinates[1])-1, "o")
-    for i in range(4):
-        print()
-    printDisplay()
+    while(True):
+        x = input("player x type coordinate (row,col): ")
+        coordinates = x.split(",")
+        if isLegal(int(coordinates[0])-1,int(coordinates[1])-1,"x",board):
+            drawSymbol(int(coordinates[0])-1,int(coordinates[1])-1,"x")
+            board[int(coordinates[0])-1][int(coordinates[1])-1] = "x"
+            for i in range(4):
+                print()
+            printDisplay()
+            break
+        else:
+            print("invalid input!")
+    while(True):
+        o = input("player o type coordinate (row,col): ")
+        coordinates = o.split(",")
+        if isLegal(int(coordinates[0])-1,int(coordinates[1])-1,"o",board):
+            drawSymbol(int(coordinates[0])-1, int(coordinates[1])-1, "o")
+            board[int(coordinates[0]) - 1][int(coordinates[1]) - 1] = "o"
+            for i in range(4):
+                print()
+            printDisplay()
+            break
+        else:
+            print("invalid input!")
 
 #change
 #git status
